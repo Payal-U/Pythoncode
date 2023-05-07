826ee1b2af0240a5ba2007c48bbc9a1d444d1fc4
Payal Upare <126785998+Payal-U@users.noreply.github.com>
2023-05-08 00:45:04 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,17 @@
+#program to perform operations between two numbers
+n1=int(input("Enter first number:"))
+n2=int(input("Enter second number:"))
+
+add=n1+n2
+print("The sum is:",add)
+substract=n1-n2
+print("The difference is:",substract)
+Multiplication=n1*n2
+print("Product is:",Multiplication)
+Division=n1/n2
+print("Dividing we get:",Division)
+Modulus=n1%n2
+print("Modulus result is:",Modulus)
+Exponent=n1**n2
+print("Exponent value is:",Exponent)
+       
