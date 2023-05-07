63c4be893e392c1a81176987a0a629a705c75e5b
Payal Upare <126785998+Payal-U@users.noreply.github.com>
2023-05-08 00:44:26 +0530

    Add files via upload

---
+++ selfintroduction.py
@@ -0,0 +1,5 @@
+#self introduction
+print("My name is Payal Upare.")
+print("I have completed my graduation from priyadarshini institute of engineering and technology in Biotechnology in 2020 followed by post graduation in Bio-informatics from university of Hyderabad in 2022.")
+print("Currently I am working as biology faculty at seeker's education inst. Tamil Nadu.")
+
